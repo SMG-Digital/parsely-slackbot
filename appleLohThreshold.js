eab94a213a4e2827c89930254332062f61beeed5
mwx1993 <mawenxin52@gmail.com>
2017-09-22 17:29:28 -0400

    CQ-7546 create slackPost file to reduce repeating code

--- appleLohThreshold.js
+++ appleLohThreshold.js
@@ -1,30 +1,8 @@
 var appleLohShares = require('./appleLohFetch').appleLohShares;
+var getSlackPost = require('./getSlackPost');
 module.exports.appleLohThreshold = function appleLohThreshold(link, text, slack, thumb_url_medium, author, hits) {
   appleLohShares(link).then(function(shares) {
-    slack.webhook({
-      channel: '#parselybot',
-      username: 'thestar bot',
-      'attachments':[
-        {
-           'fallback':text,
-           'pretext':text,
-           'thumb_url': thumb_url_medium,
-           'color':'#2ecc71',
-           'fields':[
-             {
-               'title': 'Author: ' + author,
-               'value': 'Hits: ' + (hits ? hits : 'N/A'),
-               'short':'false',
-             },
-             {
-               'title': 'shares',
-               'value': 'Twitter: ' + (shares[0].tw ? shares[0].tw : 'N/A') + ', Facebook: ' + (shares[0].fb ? shares[0].fb : 'N/A'),
-               'short': 'true'
-             }
-           ]
-        }
-     ]
-    }, function(err, response) {
+    slack.webhook(getSlackPost(text, thumb_url_medium, author, hits, shares), function(err, response) {
       console.log(response);
     });
   });
--- popularThreshold.js
+++ popularThreshold.js
@@ -1,30 +1,8 @@
 var popularShares = require('./popularFetch').popularShares;
+var getSlackPost = require('./getSlackPost');
 module.exports.popularThreshold = function popularThreshold(link, text, slack, thumb_url_medium, author, hits) {
   popularShares(link).then(function(shares) {
-    slack.webhook({
-      channel: '#parselybot',
-      username: 'thestar bot',
-      'attachments':[
-        {
-           'fallback':text,
-           'pretext':text,
-           'thumb_url': thumb_url_medium,
-           'color':'#2ecc71',
-           'fields':[
-             {
-               'title': 'Author: ' + author,
-               'value': 'Hits: ' + (hits ? hits : 'N/A'),
-               'short':'false',
-             },
-             {
-               'title': 'shares',
-               'value': 'Twitter: ' + (shares[0].tw ? shares[0].tw : 'N/A') + ', Facebook: ' + (shares[0].fb ? shares[0].fb : 'N/A'),
-               'short': 'true'
-             }
-           ]
-        }
-     ]
-    }, function(err, response) {
+    slack.webhook(getSlackPost(text, thumb_url_medium, author, hits, shares), function(err, response) {
       console.log(response);
     });
   });
